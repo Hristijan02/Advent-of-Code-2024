0f759abc16c2f24b692378f9c437d0b6f604c882
Hristijan Jovanoski <hj3103@student.uni-lj.si>
2024-12-02 06:56:13 +0100

    Day 2

---
+++ day 2/part1.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func isSafe(report []int) bool {
+	isIncreasing := true
+	isDecreasing := true
+
+	for i := 0; i < len(report)-1; i++ {
+		diff := report[i+1] - report[i]
+		temp_diff := diff
+		diff = abs(diff)
+		if diff > 3 || diff == 0 {
+			return false
+		}
+		if temp_diff < 0 {
+			isIncreasing = false
+		}
+		if temp_diff > 0 {
+			isDecreasing = false
+		}
+
+	}
+
+	isSafe := isIncreasing || isDecreasing
+	return isSafe
+}
+
+func countSafeReports(data [][]int) int {
+	count := 0
+
+	for _, report := range data {
+		if isSafe(report) {
+			count++
+		}
+	}
+
+	return count
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	var data [][]int
+
+	for {
+		scanner.Scan()
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+
+		strNumbers := strings.Fields(line)
+		var report []int
+		for _, str := range strNumbers {
+			num, err := strconv.Atoi(str)
+			if err != nil {
+				return
+			}
+			report = append(report, num)
+		}
+		data = append(data, report)
+	}
+
+	safeCount := countSafeReports(data)
+	fmt.Println(safeCount)
+
+}
